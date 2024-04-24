6ae9e8ba30022f2dcdef38b99ee3d9d88a9ff011
Nate Vaughan <nate.vaughan@protonmail.com>
2024-04-24 11:04:41 -0500

    Java8 Stream solution... better or worse?

--- src/main/java/com/natevaughan/javagym/Staircase.java
+++ src/main/java/com/natevaughan/javagym/Staircase.java
@@ -1,22 +1,19 @@
 package com.natevaughan.javagym;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Staircase {
 	public static List<String> create(Integer size) {
-		var rows = new ArrayList<String>();
-		for (var i = 0; i < size; i++) {
-			var sb = new StringBuilder();
-			for (var j = 0; j < size; j++) {
-				if (j < size - i - 1) {
-					sb.append(" ");
+		return IntStream.rangeClosed(1, size).mapToObj(i -> {
+			return IntStream.rangeClosed(1, size).mapToObj(j -> {
+				if (j < size - i + 1) {
+					return " ";
 				} else {
-					sb.append("#");
+					return "#";
 				}
-			}
-			rows.add(sb.toString());
-		}
-		return rows;
+			}).collect(Collectors.joining());
+		}).collect(Collectors.toList());
 	}
 }
