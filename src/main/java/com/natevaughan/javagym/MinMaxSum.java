b69b25b9ef11c342b9a213b56e6d0b7bb12a9288
Nate Vaughan <nate.vaughan@protonmail.com>
2024-04-24 13:47:33 -0500

    MinMaxSum and test

--- src/main/java/com/natevaughan/javagym/MinMaxSum.java
+++ src/main/java/com/natevaughan/javagym/MinMaxSum.java
@@ -3,10 +3,10 @@ package com.natevaughan.javagym;
 import java.util.Arrays;
 
 public class MinMaxSum {
-	public static Integer[] calculate(Integer[] numbers) {
+	public static Long[] calculate(Integer[] numbers) {
 		var max = Arrays.stream(numbers).max(Integer::compareTo).orElse(0);
 		var min = Arrays.stream(numbers).min(Integer::compareTo).orElse(0);
-		var sum = Arrays.stream(numbers).reduce(0, Integer::sum);
-		return new Integer[] {sum - max, sum - min};
+		var sum = Arrays.stream(numbers).map(Long::valueOf).reduce(0L, Long::sum);
+		return new Long[] {sum - max, sum - min};
 	}
 }
--- src/test/java/com/natevaughan/javagym/MinMaxSumTest.java
+++ src/test/java/com/natevaughan/javagym/MinMaxSumTest.java
@@ -12,4 +12,18 @@ public class MinMaxSumTest {
 		assertEquals(16, sums[0]);
 		assertEquals(24, sums[1]);
 	}
+	@Test
+	public void test2() {
+		var numberStream = new Integer[] {1, 2, 3, 4, 5};
+		var sums = MinMaxSum.calculate(numberStream);
+		assertEquals(10L, sums[0]);
+		assertEquals(14L, sums[1]);
+	}
+	@Test
+	public void testBig() {
+		var numberStream = new Integer[] {Integer.MAX_VALUE - 1, Integer.MAX_VALUE - 2, Integer.MAX_VALUE - 3, Integer.MAX_VALUE - 4, Integer.MAX_VALUE - 5};
+		var sums = MinMaxSum.calculate(numberStream);
+		assertEquals(Integer.MAX_VALUE * 4L - 14L, sums[0]);
+		assertEquals(Integer.MAX_VALUE * 4L - 10L, sums[1]);
+	}
 }
