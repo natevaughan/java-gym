9315f78c0e0ac5c39919d165b5319b57bb0a3dd1
Nate Vaughan <nate.vaughan@protonmail.com>
2024-04-24 21:16:57 -0500

    Time Converter before solution

---
+++ src/main/java/com/natevaughan/javagym/TimeConverter.java
@@ -0,0 +1,7 @@
+package com.natevaughan.javagym;
+
+public class TimeConverter {
+	public static String militaryFromAMPM(String time) {
+		return time;
+	}
+}
---
+++ src/test/java/com/natevaughan/javagym/TimeConverterTest.java
@@ -0,0 +1,27 @@
+package com.natevaughan.javagym;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TimeConverterTest {
+	@Test
+	public void convertOneDigitHourPM() {
+		assertEquals("19:05:45", TimeConverter.militaryFromAMPM("07:05:45PM"));
+	}
+
+	@Test
+	public void convertOneDigitHourAM() {
+		assertEquals("07:05:45", TimeConverter.militaryFromAMPM("07:05:45PM"));
+	}
+
+	@Test
+	public void convertTwelveAM() {
+		assertEquals("00:05:45", TimeConverter.militaryFromAMPM("12:05:45AM"));
+	}
+
+	@Test
+	public void convertTwelvePM() {
+		assertEquals("12:05:45", TimeConverter.militaryFromAMPM("12:05:45PM"));
+	}
+}
