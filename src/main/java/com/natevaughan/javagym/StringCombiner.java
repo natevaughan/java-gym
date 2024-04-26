6a8bb016cf19d3b1bff4db40f5c19c2f376227c9
Nate Vaughan <nate.vaughan@protonmail.com>
2024-04-25 22:10:07 -0500

    Use a set for combinations

--- src/main/java/com/natevaughan/javagym/StringCombiner.java
+++ src/main/java/com/natevaughan/javagym/StringCombiner.java
@@ -2,13 +2,14 @@ package com.natevaughan.javagym;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class StringCombiner {
-	public static List<String> combinations(List<String> strings) {
-		List<String> combinations = new ArrayList<>();
+	public static List<Set<String>> combinations(List<String> strings) {
+		List<Set<String>> combinations = new ArrayList<>();
 		for (var i = 0; i < strings.size(); i++) {
 			for (var j = i + 1; j < strings.size(); j++) {
-				combinations.add(strings.get(i) + strings.get(j));
+				combinations.add(Set.of(strings.get(i), strings.get(j)));
 			}
 		}
 		return combinations;
--- src/test/java/com/natevaughan/javagym/StringCombinationsTest.java
+++ src/test/java/com/natevaughan/javagym/StringCombinationsTest.java
@@ -3,6 +3,7 @@ package com.natevaughan.javagym;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,21 +12,21 @@ public class StringCombinationsTest {
 	@Test
 	public void stringCombinationsTest() {
 		var result = StringCombiner.combinations(List.of("a", "b", "c"));
-		assertTrue(result.contains("ab"));
-		assertTrue(result.contains("ac"));
-		assertTrue(result.contains("bc"));
+		assertTrue(result.contains(Set.of("a", "b")));
+		assertTrue(result.contains(Set.of("a", "c")));
+		assertTrue(result.contains(Set.of("b", "c")));
 		assertEquals(3, result.size());
 	}
 
 	@Test
 	public void stringCombinationsTest2() {
 		var result = StringCombiner.combinations(List.of("z", "q", "r", "s"));
-		assertTrue(result.contains("zq"));
-		assertTrue(result.contains("zr"));
-		assertTrue(result.contains("zs"));
-		assertTrue(result.contains("qr"));
-		assertTrue(result.contains("qs"));
-		assertTrue(result.contains("rs"));
+		assertTrue(result.contains(Set.of("z", "q")));
+		assertTrue(result.contains(Set.of("z", "r")));
+		assertTrue(result.contains(Set.of("z", "s")));
+		assertTrue(result.contains(Set.of("q", "r")));
+		assertTrue(result.contains(Set.of("q", "s")));
+		assertTrue(result.contains(Set.of("r", "s")));
 		assertEquals(6, result.size());
 	}
 }
