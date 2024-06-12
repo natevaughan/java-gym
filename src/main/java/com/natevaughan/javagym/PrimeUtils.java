cb2463f54d471f172f04e1bb40b1fed030dbf915
Nate Vaughan <nate.vaughan@protonmail.com>
2024-06-11 21:59:38 -0500

    PrimeUtils and test

--- src/main/java/com/natevaughan/javagym/PrimeUtils.java
+++ src/main/java/com/natevaughan/javagym/PrimeUtils.java
@@ -1,6 +1,8 @@
 package com.natevaughan.javagym;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PrimeUtils {
 	private static final List<Integer> COMMON_PRIMES = List.of(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 );
@@ -17,5 +19,23 @@ public class PrimeUtils {
 			}
 		}
 		throw new RuntimeException("No Integer prime found!");
+	}
+
+	public static Map<Integer, Integer> recursivelyFindPrimeFactors(Integer i) {
+		return recursivelyFindPrimeFactors(i, new HashMap<>());
+	}
+
+	private static Map<Integer, Integer> recursivelyFindPrimeFactors(Integer i, Map<Integer, Integer> factorsSoFar) {
+		var next = firstPrimeFactor(i);
+		if (factorsSoFar.containsKey(next)) {
+			factorsSoFar.put(next, factorsSoFar.get(next) + 1);
+		} else {
+			factorsSoFar.put(next, 1);
+		}
+		if (i / next == 1) {
+			return factorsSoFar;
+		} else {
+			return recursivelyFindPrimeFactors(i / next, factorsSoFar);
+		}
 	}
 }
--- src/test/java/com/natevaughan/javagym/PrimeUtilsTest.java
+++ src/test/java/com/natevaughan/javagym/PrimeUtilsTest.java
@@ -3,8 +3,9 @@ package com.natevaughan.javagym;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class PrimUtilsTest {
+public class PrimeUtilsTest {
     @Test
     public void findPrimesTest1() {
         var prime = PrimeUtils.firstPrimeFactor(10);
@@ -15,5 +16,20 @@ public class PrimUtilsTest {
     public void findPrimesTestAbove100() {
         var prime = PrimeUtils.firstPrimeFactor(749753);
         assertEquals(823, prime);
+    }
+
+    @Test
+    public void recursivelyFindPrimesTestUniquePrimes() {
+        var factors = PrimeUtils.recursivelyFindPrimeFactors(10);
+        assertEquals(2, factors.size());
+    }
+
+    @Test
+    public void findPrimesWithDuplicates() {
+        var factors = PrimeUtils.recursivelyFindPrimeFactors(12);
+        assertEquals(2, factors.size());
+        assertEquals(2, factors.get(2));
+        assertEquals(1, factors.get(3));
+        assertNull(factors.get(5));
     }
 }
