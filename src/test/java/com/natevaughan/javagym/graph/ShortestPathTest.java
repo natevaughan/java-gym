f8ffccfdaceba78aa9046c303fd36d38159fad40
Nate Vaughan <nate.vaughan@protonmail.com>
2024-06-20 00:35:27 -0500

    Update shortestPathTest

--- src/test/java/com/natevaughan/javagym/graph/ShortestPathTest.java
+++ src/test/java/com/natevaughan/javagym/graph/ShortestPathTest.java
@@ -22,4 +22,75 @@ public class ShortestPathTest {
 		);
 		assertEquals(3, ShortestPath.calculate(graph, "Albuqurque", "Las Vegas"));
 	}
+
+	@Test
+	public void test2() {
+		List<List<String>> edges = List.of(
+				List.of("w", "x"),
+				List.of("x", "y"),
+				List.of("z", "y"),
+				List.of("z", "v"),
+				List.of("w", "v")
+		);
+
+		assertEquals(2, ShortestPath.calculate(edges, "w", "z"));
+	}
+
+	@Test
+	public void test3() {
+		List<List<String>> edges = List.of(
+				List.of("w", "x"),
+				List.of("x", "y"),
+				List.of("z", "y"),
+				List.of("z", "v"),
+				List.of("w", "v")
+		);
+
+		assertEquals(1, ShortestPath.calculate(edges, "y", "x"));
+	}
+
+	@Test
+	public void test4() {
+		List<List<String>> edges = List.of(
+				List.of("a", "c"),
+				List.of("a", "b"),
+				List.of("c", "b"),
+				List.of("c", "d"),
+				List.of("b", "d"),
+				List.of("e", "d"),
+				List.of("g", "f")
+		);
+
+		assertEquals(3, ShortestPath.calculate(edges, "a", "e"));
+	}
+
+	@Test
+	public void test5() {
+		List<List<String>> edges = List.of(
+				List.of("a", "c"),
+				List.of("a", "b"),
+				List.of("c", "b"),
+				List.of("c", "d"),
+				List.of("b", "d"),
+				List.of("e", "d"),
+				List.of("g", "f")
+		);
+
+		assertEquals(2, ShortestPath.calculate(edges, "e", "c"));
+	}
+
+	@Test
+	public void test6() {
+		List<List<String>> edges = List.of(
+				List.of("m", "n"),
+				List.of("n", "o"),
+				List.of("o", "p"),
+				List.of("p", "q"),
+				List.of("t", "o"),
+				List.of("r", "q"),
+				List.of("r", "s")
+		);
+
+		assertEquals(6, ShortestPath.calculate(edges, "m", "s"));
+	}
 }
