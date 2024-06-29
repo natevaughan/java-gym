4cfec9b0d81936e606bad2a38e6d29955a1153f2
Nate Vaughan <nate.vaughan@protonmail.com>
2024-06-28 22:32:52 -0500

    Impl restaurant

--- src/main/java/com/natevaughan/javagym/oo/strategy/Restaurant.java
+++ src/main/java/com/natevaughan/javagym/oo/strategy/Restaurant.java
@@ -1,7 +1,104 @@
 package com.natevaughan.javagym.oo.strategy;
 
+import java.util.*;
+
 public class Restaurant {
+	public static class MItem {
+		String id;
+		String name;
+		Double price;
+		public MItem(String id, String name, Double price) {
+			this.id = id;
+			this.name = name;
+			this.price = price;
+		}
+	}
+
+	public static class RTable {
+		String id;
+		Integer cap;
+		Integer seated = 0;
+		public RTable(String id, Integer cap) {
+			this.id = id;
+			this.cap = cap;
+		}
+	}
+
+	/**
+	 * State
+	 */
+	Map<String, MItem> items = new HashMap<>();
+	Set<String> itemNames = new HashSet<>();
+	Map<String, RTable> tables = new HashMap<>();
+
 	public String process(String command) {
+		var parts = parseCommand(command);
+		var method = parts.get(0);
+		if ("CREATE_ITEM".equals(method)) {
+			return createItem(parts.get(1), parts.get(2), Double.parseDouble(parts.get(3)));
+		} else if ("CREATE_TABLE".equals(method)) {
+			return createTable(parts.get(1), Integer.parseInt(parts.get(2)));
+		} else if ("SEAT_PARTY".equals(method)) {
+			String tableId = null;
+			if (parts.size() > 2) {
+				tableId = parts.get(2);
+			}
+			return seatParty(Integer.parseInt(parts.get(1)), tableId);
+		} else if ("ORDER_ITEM".equals(method)) {
+			return orderItem(parts.get(1), parts.get(2));
+		} else if ("CHECKOUT".equals(method)) {
+			return checkout(parts.get(1));
+		}
 		return command;
+	}
+
+	private String checkout(String tableId) {
+		return null;
+	}
+
+	private String orderItem(String itemId, String tableId) {
+		return null;
+	}
+
+	private String seatParty(Integer size, String tableId) {
+		RTable matched = null;
+		if (tableId != null) {
+			if (!tables.containsKey(tableId)) {
+				return "nonexistant";
+			}
+			matched = tables.get(tableId);
+		}
+		if (matched.seated > 0) {
+			return "occupied";
+		}
+		if (matched.cap < size) {
+			return "insufficient";
+		}
+		matched.seated = size;
+		return null;
+	}
+
+	private String createTable(String tableId, Integer cap) {
+		if (tables.containsKey(tableId)) {
+			return "duplicate";
+		}
+		tables.put(tableId, new RTable(tableId, cap));
+		return "created";
+	}
+
+	private String createItem(String itemId, String itemName, Double price) {
+		if (items.containsKey(itemId)) {
+			return "duplicate";
+		}
+		if (itemNames.contains(itemName)) {
+			return "duplicate";
+		}
+		itemNames.add(itemName);
+		items.put(itemId, new MItem(itemId, itemName, price));
+		return "created";
+	}
+
+	private List<String> parseCommand(String c) {
+		return List.of(c.split(" "));
 	}
 }
--- src/test/java/com/natevaughan/javagym/oo/strategy/RestaurantTest.java
+++ src/test/java/com/natevaughan/javagym/oo/strategy/RestaurantTest.java
@@ -7,33 +7,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RestaurantTest {
 	/**
 	 * Create a class called Restaurant that takes the following commands
-	 * CREATE_MENU_ITEM <item_id> <item_name> <price>
+	 * CREATE_ITEM <item_id> <item_name> <price>
 	 *     -> "created" if success
 	 *     -> "duplicate" if duplicated name or id
 	 * CREATE_TABLE <table_id> <seat_count>
 	 *     -> "created" if success
-	 *     -> "duplicate" if duplicated id
+	 *     -> "duplicate" if duplicated table_id
 	 * SEAT_PARTY <guest_count> <table_id_optional>
 	 *     -> seat the party at the optionally specified table, or choose a table that can acommodate the guests
 	 *     -> <table_id> if there is a table that can accommodate the guests or if the table specified is available
 	 *     -> "occupied" if the table specified is occupied
+	 *     -> "insufficient" if there are not enough seats at the table
 	 *     -> "" if there is no table available
 	 * ORDER_ITEM <item_id> <table_number>
 	 *     -> "ordered" if success
-	 *     -> "nonexistant" if the item doesn't exist
+	 *     -> "nonexistant" if the item doesn't exist or table doesn't exist
 	 *     -> "unsat" if the table does not have guests
 	 * CHECKOUT <table_id>
-	 *     -> Return the total amount owed to settle the bill, with 10.5% tax
-	 *     -> Include 18% gratuity if there are more than 5 in the party
+	 *     -> Return the total amount owed to settle the bill, with 8% tax
+	 *     -> Include 15% gratuity (on pre-tax total) if there are more than 5 in the party
 	 * CAPACITY
 	 *     -> "70.0%" return the percent, out of 100, with 1 decimal place, of seats filled in the restaurant
 	 */
 	@Test
 	public void restaurantTest1() {
 		Restaurant r = new Restaurant();
-		assertEquals("created", r.process("CREATE_MENU_ITEM app01 SpinDip 9.50"));
-		assertEquals("duplicate", r.process("CREATE_MENU_ITEM app01 Fries 6.50"));
-		assertEquals("duplicate", r.process("CREATE_MENU_ITEM app02 SpinDip 8.50"));
+		assertEquals("created", r.process("CREATE_ITEM app01 SpinDip 9.50"));
+		assertEquals("duplicate", r.process("CREATE_ITEM app01 Fries 6.50"));
+		assertEquals("duplicate", r.process("CREATE_ITEM app02 SpinDip 8.50"));
 	}
 
 	@Test
@@ -49,12 +50,75 @@ public class RestaurantTest {
 		assertEquals("created", r.process("CREATE_TABLE table01 4"));
 		assertEquals("created", r.process("CREATE_TABLE table02 6"));
 		assertEquals("created", r.process("CREATE_TABLE table03 2"));
-		assertEquals("table01", r.process("SEAT_PARTY 5"));
+		assertEquals("duplicate", r.process("CREATE_TABLE table01 4"));
+		assertEquals("table02", r.process("SEAT_PARTY 5"));
 		assertEquals("table03", r.process("SEAT_PARTY 1"));
 		assertEquals("", r.process("SEAT_PARTY 5"));
+		assertEquals("table01", r.process("SEAT_PARTY 3"));
 		assertEquals("occupied", r.process("SEAT_PARTY 5 table01"));
 	}
 
+	@Test
+	public void restaurantTest3_2() {
+		Restaurant r = new Restaurant();
+		assertEquals("created", r.process("CREATE_TABLE table01 4"));
+		assertEquals("nonexistant", r.process("SEAT_PARTY 5 table02"));
+	}
+
+	@Test
+	public void restaurantTest3_3() {
+		Restaurant r = new Restaurant();
+		assertEquals("created", r.process("CREATE_TABLE table01 4"));
+		assertEquals("insufficient", r.process("SEAT_PARTY 5 table01"));
+	}
+
+	@Test
+	public void restaurantTest4() {
+		Restaurant r = new Restaurant();
+		assertEquals("created", r.process("CREATE_ITEM app01 SpinDip 9.55"));
+		assertEquals("nonexistant", r.process("ORDER_ITEM app01 table01"));
+	}
+
+	@Test
+	public void restaurantTest5() {
+		Restaurant r = new Restaurant();
+		assertEquals("created", r.process("CREATE_TABLE table01 4"));
+		assertEquals("nonexistant", r.process("ORDER_ITEM app01 table01"));
+	}
+
+	@Test
+	public void restaurantTest6() {
+		Restaurant r = new Restaurant();
+		assertEquals("created", r.process("CREATE_ITEM app01 SpinDip 9.55"));
+		assertEquals("created", r.process("CREATE_ITEM entr02 Burger 15.90"));
+		assertEquals("created", r.process("CREATE_TABLE table01 4"));
+		assertEquals("table01", r.process("SEAT_PARTY 3"));
+		assertEquals("ordered", r.process("ORDER_ITEM app01 table01"));
+		assertEquals("ordered", r.process("ORDER_ITEM entr02 table01"));
+		assertEquals("ordered", r.process("ORDER_ITEM entr02 table01"));
+		assertEquals("ordered", r.process("ORDER_ITEM entr02 table01"));
+		assertEquals("$61.83", r.process("CHECKOUT table01"));
+	}
+
+	@Test
+	public void restaurantTest7() {
+		Restaurant r = new Restaurant();
+		assertEquals("created", r.process("CREATE_ITEM app01 SpinDip 9.55"));
+		assertEquals("created", r.process("CREATE_ITEM app02 Fries 6.90"));
+		assertEquals("created", r.process("CREATE_ITEM entr01 Salmon 22.95"));
+		assertEquals("created", r.process("CREATE_ITEM entr02 Burger 15.90"));
+		assertEquals("created", r.process("CREATE_TABLE table02 6"));
+		assertEquals("table01", r.process("SEAT_PARTY 6"));
+		assertEquals("ordered", r.process("ORDER_ITEM app01 table02"));
+		assertEquals("ordered", r.process("ORDER_ITEM app02 table02"));
+		assertEquals("ordered", r.process("ORDER_ITEM entr02 table02"));
+		assertEquals("ordered", r.process("ORDER_ITEM entr02 table02"));
+		assertEquals("ordered", r.process("ORDER_ITEM entr02 table02"));
+		assertEquals("ordered", r.process("ORDER_ITEM entr01 table02"));
+		assertEquals("ordered", r.process("ORDER_ITEM entr01 table02"));
+		assertEquals("ordered", r.process("ORDER_ITEM entr01 table02"));
+		assertEquals("$163.59", r.process("CHECKOUT table02"));
+	}
 
 	/**
 	 * Now create a new command that gets the sales totals for each item and
